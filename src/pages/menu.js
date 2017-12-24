834a70e1b0df9ccd7a0d0a982621b62a7ac96eb8
Kyle Gill <kylerobertgill@gmail.com>
2017-12-23 17:43:45 -0700

    data import from Google Sheets

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -4,16 +4,51 @@ import React from "react";
 import Features from "../components/features";
 import MenuItems from "../components/page-blocks/menu-items";
 import Nav from "../components/nav";
+import GetSheetDone from "get-sheet-done";
 
-const MenuPage = props => (
-  <main>
-    <Nav />
-    <MenuItems data={props.data.allDataJson.edges[0].node.menu} />
-  </main>
-);
+class MenuPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      entrees: [],
+      drinks: []
+    };
+  }
+
+  componentDidMount() {
+    GetSheetDone.labeledCols(
+      "1y9YVhJJCYs7NBj2a-aql7JJPFI6mmXW06efDaXBGYkc",
+      1
+    ).then(sheet => {
+      console.log(sheet);
+      this.setState({ entrees: sheet.data });
+    });
+    GetSheetDone.labeledCols(
+      "1y9YVhJJCYs7NBj2a-aql7JJPFI6mmXW06efDaXBGYkc",
+      2
+    ).then(sheet => {
+      console.log(sheet);
+      this.setState({ drinks: sheet.data });
+    });
+  }
+
+  render() {
+    return (
+      <main>
+        <Nav />
+        <div style={{ height: 70 }} />
+        {/* <MenuItems data={this.props.data.allDataJson.edges[0].node.menu} /> */}
+        <MenuItems data={this.state.entrees} title="Entrees" />
+        <MenuItems data={this.state.drinks} title="Drinks" />
+        <div style={{ height: 70 }} />
+      </main>
+    );
+  }
+}
 
 export default MenuPage;
 
+// used with the first commented out line of Menu Items
 export const pageQuery = graphql`
   query MenuQuery {
     allDataJson {
